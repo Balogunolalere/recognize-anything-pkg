bd500586fcbc9340f8156f19416cc6239b73886d
Balogunolalere <38155412+Balogunolalere@users.noreply.github.com>
2024-09-01 17:36:49 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,29 @@
-import setuptools
-setuptools.setup()
+from setuptools import setup, find_packages
+
+setup(
+    name='recognize_anything',
+    version='0.1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'timm',
+        'transformers',
+        'fairscale',
+        'pycocoevalcap'
+    ],
+    entry_points={
+        'console_scripts': [
+            'recognize_anything=recognize_anything.inference_ram:main',
+        ],
+    },
+    package_data={
+        'recognize_anything': [
+            'datasets/*',
+            'images/*',
+            'ram/configs/*',
+            'ram/data/*',
+            'ram/models/*',
+            'ram/utils/*'
+        ]
+    },
+)
